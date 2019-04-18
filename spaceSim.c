ba495f1635fc50e7588dee1bda2221021650f262
farrer <farrer@rpi.edu>
2019-04-17 23:41:42 -0400

    Added an index for Bodies, made starting object positions random, and added MPI initialization

--- spaceSim.c
+++ spaceSim.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<math.h>
+#include <string.h>
+#include <math.h>
+#include <mpi.h>
 
 //Space Simulator
 int IDnum = 0;
@@ -37,32 +38,59 @@ void create_ID(Body *b) {
 	IDnum++;
 }
 
+//MPI variables
+int mpi_myrank;
+int mpi_commsize;
+
 //array of bodies for each rank
 Body* bodies;
+int bodies_index = 0;
 
 //universe bounds x, y, z
 int boundx, boundy, boundz;
 //tick time length
 int ticks;
 
+//Mathematical constants
+float gravity = .0000000000667408;
+int hubble = 500;	//Units km/s/Mpc
+
+
 
 
 
 int main(int argc, char** argv) {
 	//declare ranks
+	MPI_Init( &argc, &argv);
+	MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
+	MPI_Comm_rank( MPI_COMM_WORLD, &my_mpi_rank);
+	upper = argv[1] / mpi_commsize;
+
+	Bodies = calloc(3,sizeof(Body));
 
 	//how are bodies distributed randomly?
+	//Generate random ints in the range of the universe bounds as x,y,z coordinates
 	Body star;
+	Body planet;
 	Body stroid;
-	init_body(&star, "Star", 10, 10, 0, 0, 0, 1, 0, 0);
-	init_body(&stroid, "Asteroid", 10, 10, 0, 0, 0, 1, 0, 0);
+	init_body(&star, "Star", 100, 10, (rand() % (upper + 1)), (rand() % (upper + 1)) + lower, (rand() % (upper + 1)) + lower, 1, 0, 0);
+	init_body(&star, "Planet", 10, 10, (rand() % (upper + 1)), (rand() % (upper + 1)) + lower, (rand() % (upper + 1)) + lower, 1, 0, 0);
+	init_body(&stroid, "Asteroid", 1, 10, (rand() % (upper + 1)), (rand() % (upper + 1)) + lower, (rand() % (upper + 1)) + lower, 1, 0, 0);
+	Bodies[bodies_index] = star;
+	bodies_index++;
+	Bodies[bodies_index] = planet;
+	bodies_index++;
+	Bodies[bodies_index] = stroid;
+	bodies_index++;
 
 	printf("ID: %d, Type: %s, Mass: %d\n", ((&star)->ID), ((&star)->type), ((&star)->mass));
 	printf("ID: %d, Type: %s, Mass: %d\n", ((&stroid)->ID), ((&stroid)->type), ((&stroid)->mass));
 		//declare number, location, and velocity of objects
 
 	//for each tick
-		//calculate 
+		//calculate
+
+
+	//After calculating, pass objects between ranks
+
 }
-
-
